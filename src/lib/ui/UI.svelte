89b660ed5650330a14ea8acc98b5b5ad83bb8dbe
mattjennings <me@mattjennings.io>
2022-08-05 19:47:52 -0600

    style ui

--- src/lib/ui/UI.svelte
+++ src/lib/ui/UI.svelte
@@ -20,17 +20,19 @@
 </script>
 
 <div class="root">
-  <div class="score">
-    {#if !playing}
-      <span>HIGHSCORE: {highscore}</span>
-    {/if}
+  <div class="scores">
+    <span class="highscore">HIGHSCORE: {highscore}</span>
 
-    <span>{score}</span>
-
-    {#if !playing && !dead}
-      <span>TAP TO START</span>
+    {#if score > 0}
+      <span class="score">{score}</span>
     {/if}
   </div>
+
+  {#if !playing && !dead}
+    <div class="tap-to-start">
+      <span>TAP TO START</span>
+    </div>
+  {/if}
 </div>
 
 <style>
@@ -40,9 +42,40 @@
   }
 
   .root {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 100%;
     font-family: Pixel;
     pointer-events: none;
     color: white;
-    font-size: 32em;
+    font-size: 28em;
+
+    --outline-width: calc(2 * var(--px));
+    --n-outline-width: calc(-2 * var(--px));
+
+    text-shadow: var(--n-outline-width) var(--n-outline-width) 0 #000,
+      var(--outline-width) var(--n-outline-width) 0 #000,
+      var(--n-outline-width) var(--outline-width) 0 #000,
+      var(--outline-width) var(--outline-width) 0 #000;
+  }
+
+  .scores {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: calc(48 * var(--px));
+    padding: calc(32 * var(--px));
+  }
+
+  .score {
+    font-size: 1.5em;
+  }
+
+  .tap-to-start {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: calc(16 * var(--px));
   }
 </style>
